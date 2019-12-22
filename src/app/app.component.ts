70e9248ba2881d8a2db2d78b2b09a56ec0c6b7f3
irek02 <imirgaleev@gmail.com>
2019-12-22 15:49:06 -0800

    revert changes in master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import * as moment from 'moment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,24 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = (new Foo()).getName();
-  shippingOptions = ['overnight', 'standard'];
-  selectedOption = null;
-  estimatedDelivery = null;
-
-  select(option) {
-    this.selectedOption = option;
-    if (option == 'overnight') {
-      this.estimatedDelivery = moment().add(1, 'day').format('MMMM D, Y');
-    }
-    else if (option == 'standard') {
-      this.estimatedDelivery = moment().add(3, 'day').format('MMMM D, Y');
-    }
-  }
-}
-
-class Foo {
-  getName() {
-    return 'hello';
-  }
+  title = 'Design Patterns';
 }
