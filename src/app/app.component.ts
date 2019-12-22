641448b44c1b547199e618a7aceca5eea084c18a
irek02 <imirgaleev@gmail.com>
2019-12-21 16:44:51 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'design-patterns';
+  title = (new Foo()).getName();
+  shippingOptions = ['overnight', 'standard'];
+  selectedOption = null;
+  estimatedDelivery = null;
+
+  select(option) {
+    this.selectedOption = option;
+    if (option == 'overnight') {
+      this.estimatedDelivery = moment().add(1, 'day').format('MMMM D, Y');
+    }
+    else if (option == 'standard') {
+      this.estimatedDelivery = moment().add(3, 'day').format('MMMM D, Y');
+    }
+  }
+}
+
+class Foo {
+  getName() {
+    return 'hello';
+  }
 }
