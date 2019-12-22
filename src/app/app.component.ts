f7bb4d912cbae30e81e900bd3870dcf4d966c702
irek02 <imirgaleev@gmail.com>
2019-12-22 15:46:58 -0800

    refact to use the strategy pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = (new Foo()).getName();
-  shippingOptions = ['overnight', 'standard'];
+  title = 'Strategy';
+  shippingOptions = ['express', 'standard'];
   selectedOption = null;
   estimatedDelivery = null;
 
-  select(option) {
+  /////////////////////////////
+  ////////// BEFORE ///////////
+  /////////////////////////////
+
+  // clickOnShippingMethod(option) {
+  //   this.selectedOption = option;
+  //   const shipping = new Shipping(option);
+  //   this.estimatedDelivery = shipping.estimateDeliveryDate();
+  // }
+
+  /////////////////////////////
+  ////////// AFTER ///////////
+  /////////////////////////////
+
+  clickOnShippingMethod(option) {
     this.selectedOption = option;
-    if (option == 'overnight') {
-      this.estimatedDelivery = moment().add(1, 'day').format('MMMM D, Y');
+    let strategy;
+    if (option == 'express') {
+      strategy = new ExpressShippingStrategy();
     }
     else if (option == 'standard') {
-      this.estimatedDelivery = moment().add(3, 'day').format('MMMM D, Y');
+      strategy = new StandardShippingStrategy();
     }
+    const shipping = new Shipping(strategy);
+    this.estimatedDelivery = shipping.estimateDeliveryDate();
   }
 }
 
-class Foo {
-  getName() {
-    return 'hello';
+/////////////////////////////
+////////// BEFORE ///////////
+/////////////////////////////
+
+// class Shipping {
+//   constructor(public option) { }
+//   estimateDeliveryDate() {
+//     if (this.option == 'express') {
+//       // Express shipping takes about two days.
+//       return moment().add(2, 'day').format('MMMM D, Y');
+//     }
+//     else if (this.option == 'standard') {
+//       // Standard shipping takes about 5 days.
+//       return moment().add(5, 'day').format('MMMM D, Y');
+//     }
+//   }
+// }
+
+/////////////////////////////
+////////// AFTER ///////////
+/////////////////////////////
+
+interface ShippingStrategy {
+  estimateDeliveryDate(): string;
+}
+
+class ExpressShippingStrategy implements ShippingStrategy {
+  estimateDeliveryDate() {
+    // Express shipping takes about two days.
+    return moment().add(2, 'day').format('MMMM D, Y');
+  }
+}
+
+class StandardShippingStrategy implements ShippingStrategy {
+  estimateDeliveryDate() {
+    // Standard shipping takes about 5 days.
+    return moment().add(5, 'day').format('MMMM D, Y');
+  }
+}
+
+class Shipping {
+  constructor(public strategy: ShippingStrategy) { }
+  estimateDeliveryDate() {
+    return this.strategy.estimateDeliveryDate();
   }
 }
